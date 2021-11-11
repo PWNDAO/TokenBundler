284e5b094e4ba702ceea60ac2b387b510fe37831
Naim Ashhab <ashhab.naim@gmail.com>
2021-11-11 15:52:15 +0100

    Use MultiToken library as a npm package

--- contracts/Bundler.sol
+++ contracts/Bundler.sol
@@ -1,8 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-import "./MultiToken.sol";
+import "@pwnfinance/multitoken/contracts/MultiToken.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
