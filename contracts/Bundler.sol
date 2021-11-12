ecca83a8dcc1db675019d9422e8ee72b013b7e6b
Naim Ashhab <ashhab.naim@gmail.com>
2021-11-12 16:54:42 +0100

    Set contract licenses to MIT

--- contracts/Bundler.sol
+++ contracts/Bundler.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity ^0.8.0;
 
 import "@pwnfinance/multitoken/contracts/MultiToken.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
