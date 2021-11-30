77a44337341651d0c9fc3963690527f2f527d844
Naim Ashhab <ashhab.naim@gmail.com>
2021-11-30 19:32:13 +0100

    Unify indentation

--- contracts/ITokenBundler.sol
+++ contracts/ITokenBundler.sol
@@ -5,16 +5,16 @@ import "@pwnfinance/multitoken/contracts/MultiToken.sol";
 
 interface ITokenBundler {
 
-	/**
-	 * BundleCreated
-	 * @dev Emitted when new bundle is created.
-	 */
-	event BundleCreated(uint256 indexed id, address indexed creator);
+    /**
+     * BundleCreated
+     * @dev Emitted when new bundle is created.
+     */
+    event BundleCreated(uint256 indexed id, address indexed creator);
 
-	/**
-	 * BundleUnwrapped
-	 * @dev Emitted when bundle is unwrapped and burned.
-	 */
+    /**
+     * BundleUnwrapped
+     * @dev Emitted when bundle is unwrapped and burned.
+     */
     event BundleUnwrapped(uint256 indexed id);
 
     /**
@@ -25,35 +25,35 @@ interface ITokenBundler {
      * @param _assets List of assets to include in a bundle
      * @return Bundle id
      */
-	function create(MultiToken.Asset[] memory _assets) external returns (uint256);
+    function create(MultiToken.Asset[] memory _assets) external returns (uint256);
 
-	/**
+    /**
      * unwrap
      * @dev Sender has to be a bundle owner.
      * @dev Burns bundle token and transfers assets to sender.
      * @dev Emits a {BundleUnwrapped} event.
      * @param _bundleId Bundle id to unwrap
      */
-	function unwrap(uint256 _bundleId) external;
+    function unwrap(uint256 _bundleId) external;
 
-	/**
-	 * token
-	 * @param _tokenId Token nounce from bundle asset list.
-	 * @return MultiToken.Asset struct
-	 */
-	function token(uint256 _tokenId) external view returns (MultiToken.Asset memory);
+    /**
+     * token
+     * @param _tokenId Token nounce from bundle asset list.
+     * @return MultiToken.Asset struct
+     */
+    function token(uint256 _tokenId) external view returns (MultiToken.Asset memory);
 
-	/**
-	 * bundle
-	 * @param _bundleId Bundle id.
-	 * @return List of assets in a bundle.
-	 */
-	function bundle(uint256 _bundleId) external view returns (uint256[] memory);
+    /**
+     * bundle
+     * @param _bundleId Bundle id.
+     * @return List of assets in a bundle.
+     */
+    function bundle(uint256 _bundleId) external view returns (uint256[] memory);
 
-	/**
-	 * maxSize
-	 * @return Maximum bundle size.
-	 */
-	function maxSize() external view returns (uint256);
+    /**
+     * maxSize
+     * @return Maximum bundle size.
+     */
+    function maxSize() external view returns (uint256);
 
 }
