88d53f9fa0e2168591a0dab11de04b5850317ed8
Naim Ashhab <ashhab.naim@gmail.com>
2022-02-14 17:11:28 -0300

    Implement deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
   solidity: {
@@ -10,5 +12,24 @@ module.exports = {
         }
       }
     }
-  }
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.DEPLOY_PRIVATE_KEY_TESTNET !== undefined
+          ? [process.env.DEPLOY_PRIVATE_KEY_TESTNET]
+          : [],
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      accounts:
+        process.env.DEPLOY_PRIVATE_KEY_LOCAL !== undefined
+          ? [process.env.DEPLOY_PRIVATE_KEY_LOCAL]
+          : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
