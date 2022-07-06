e48f01b5f4741342e2b7c4792922c5de28cb69bd
ashhanai <ashhab.naim@gmail.com>
2022-07-06 13:45:35 +0300

    Rewrite deploy script into solidity

---
+++ script/TokenBundler.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "forge-std/Script.sol";
+import "../src/TokenBundler.sol";
+
+
+contract Deploy is Script {
+
+	function run() external {
+		vm.startBroadcast();
+
+        new TokenBundler("https://test.uri/", 40);
+
+        vm.stopBroadcast();
+	}
+
+}
