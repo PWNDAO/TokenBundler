8d0a43e5dc2b9bf04019ed79872e3e5c7b00cfe9
ashhanai <ashhab.naim@gmail.com>
2022-09-07 09:50:51 +0100

    Add comments to deployment script

--- script/TokenBundler.s.sol
+++ script/TokenBundler.s.sol
@@ -5,6 +5,16 @@ import "forge-std/Script.sol";
 import "../src/TokenBundler.sol";
 
 
+/*
+Deploy TokenBundler contracts by executing commands:
+
+source .env
+
+forge script script/TokenBundler.s.sol:Deploy \
+--rpc-url $GOERLI_URL \
+--private-key $DEPLOY_PRIVATE_KEY_TESTNET \
+--broadcast
+ */
 contract Deploy is Script {
 
 	function run() external {
